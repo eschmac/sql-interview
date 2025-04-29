6cd59bee462904ab865cb5c3c2c22a3cda56cafb
Ethan Schmitz <ethan.schmitz@smartac.com>
2025-04-29 12:47:56 -0700

    test case covering a large amount of transactions

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -67,4 +67,25 @@ def test_sql_example_2():
     for i, o in enumerate(outputs):
         if o != expected_outputs[i]:
             pytest.fail(f"step {i} should've returned {expected_outputs[i]} but returned {o}")
-            
+
+def test_example_3():
+    s = SQL(["ez","v","u","pl","rz","snlw","qk","egy","x","iyzdq","bpfef","vr"],[7,8,2,3,5,5,2,6,4,1,1,5])
+    
+    calls = ["ins","sel","sel","sel","sel","ins","sel","ins","ins","rmv","sel","ins","sel","sel","sel","sel","rmv","sel","sel","sel","ins","rmv","sel","sel","sel","sel","ins","ins","sel","ins","sel","ins","sel","sel","rmv","ins","ins","sel","ins","sel","sel","sel","ins","sel","sel","ins","sel","rmv","sel","sel","rmv","rmv","ins","sel","sel","sel","sel","sel","sel","rmv","ins","rmv","sel","sel","rmv","sel","rmv","ins","sel","rmv","ins","sel","sel","sel","ins","rmv"]
+    inputs = [["u",["lae","l"]],["u",1,2],["u",1,2],["u",1,2],["u",1,1],["ez",["kkn","dka","xrij","li","cc","g","f"]],["ez",1,5],["iyzdq",["lo"]],["vr",["y","i","f","ll","mps"]],["vr",1],["iyzdq",1,1],["qk",["lu","jeazw"]],["ez",1,6],["qk",1,1],["u",1,1],["u",1,1],["ez",1],["iyzdq",1,1],["iyzdq",1,1],["u",1,2],["bpfef",["kz"]],["iyzdq",1],["qk",1,1],["u",1,1],["qk",1,2],["u",1,2],["iyzdq",["cu"]],["x",["nrnet","fiy","pbmp","tbgs"]],["x",1,2],["iyzdq",["t"]],["iyzdq",3,1],["egy",["lg","pmtu","joqdt","psdyy","nk","gfb"]],["bpfef",1,1],["bpfef",1,1],["egy",1],["snlw",["jbu","s","xeuqs","zvvy","izogr"]],["rz",["vodbu","bpd","pa","eu","eu"]],["iyzdq",2,1],["qk",["kbjqg","y"]],["qk",2,2],["rz",1,5],["qk",1,1],["iyzdq",["hlujc"]],["rz",1,3],["iyzdq",3,1],["rz",["vw","yq","imkyf","bpv","kfne"]],["rz",1,1],["x",1],["rz",2,1],["qk",2,1],["qk",1],["iyzdq",2],["rz",["wdvcr","vpb","a","ar","xvbl"]],["rz",1,4],["iyzdq",4,1],["rz",2,4],["bpfef",1,1],["snlw",1,2],["bpfef",1,1],["rz",3],["vr",["it","tr","bgia","b","zif"]],["iyzdq",4],["bpfef",1,1],["vr",2,1],["iyzdq",3],["vr",2,3],["qk",2],["v",["onmdi","noe","sv","slyt","ruv","iscm","r","spti"]],["rz",1,3],["vr",2],["iyzdq",["ewkky"]],["rz",2,1],["iyzdq",5,1],["rz",2,4],["x",["g","zkp","skj","xng"]],["iyzdq",5]]
+    translations = {
+        "ins": s.ins,
+        "sel": s.sel,
+        "rmv": s.rmv, 
+        "exp": s.exp,
+    }
+    
+    outputs = []
+    for i, call in enumerate(calls):
+        f = translations[call]
+        outputs.append(f(*inputs[i]))
+        
+    expected_outputs = [True,"l","l","l","lae",True,"cc",True,True,None,"lo",True,"g","lu","lae","lae",None,"lo","lo","l",True,None,"lu","lae","jeazw","l",True,True,"fiy",True,"t",True,"kz","kz",None,True,True,"cu",True,"y","eu","lu",True,"pa","t",True,"vodbu",None,"vw","kbjqg",None,None,True,"eu","hlujc","bpv","kz","s","kz",None,True,None,"kz","it",None,"bgia",None,True,"pa",None,True,"vw","ewkky","bpv",True,None]
+    for i, o in enumerate(outputs):
+        if o != expected_outputs[i]:
+            pytest.fail(f"step {i} should've returned {expected_outputs[i]} but returned {o}")
